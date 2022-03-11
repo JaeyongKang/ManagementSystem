745664bf492bf0076ed5fcb378c8dab0b8134f3c
JaeyongKang <jaeyongkang@naver.com>
2022-03-11 13:47:45 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Assignment");
+		System.out.println("2. Edit Assignment");
+		System.out.println("3. Delete Assignment");
+		System.out.println("4. View Assignment");
+		System.out.println("5. Exit");
+
+	}
+
+}
