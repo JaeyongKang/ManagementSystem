ceb354c370fdb9453476ed063066f123be5b547d
JaeyongKang <jaeyongkang@naver.com>
2022-05-25 00:49:01 +0900

    added gui

---
+++ src/gui/AssignmentAdder.java
@@ -0,0 +1,43 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class AssignmentAdder extends JFrame{
+	public AssignmentAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelName = new JLabel("Name: ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelDate = new JLabel("Date: ", JLabel.TRAILING);
+		JTextField fieldDate = new JTextField(10);
+		labelDate.setLabelFor(fieldDate);
+		panel.add(labelDate);
+		panel.add(fieldDate);
+		
+		JLabel labelContent = new JLabel("Content: ", JLabel.TRAILING);
+		JTextField fieldContent = new JTextField(10);
+		labelDate.setLabelFor(fieldContent);
+		panel.add(labelContent);
+		panel.add(fieldContent);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 4, 2, 6, 6, 6, 6);
+		
+		this.setSize(300,300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
+}
---
+++ src/gui/GUITest.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class GUITest {
+
+	public static void main(String[] args) {
+		//MenuSelection menu = new MenuSelection();
+		//AssignmentAdder adder = new AssignmentAdder();
+		AssignmentViewer viewer = new AssignmentViewer();
+
+	}
+
+}
